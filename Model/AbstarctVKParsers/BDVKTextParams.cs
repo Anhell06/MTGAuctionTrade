d4e79ae8bdd8dab84e77128850b49079ac3117d7
Fedoskin Konstantin <Dlyll@mail.ru>
2020-09-04 01:26:47 +0500

    Folder for VK and hardcode params

---
+++ Model/AbstarctVKParsers/BDVKTextParams.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class BDVKTextParams
+{
+    public enum ParamTextSubsring
+    {
+        StartName,
+        StartTime,
+        StartPrice,
+        StartNowPrice,
+        EndName,
+        EndTime,
+        EndPrice,
+        EndNowPrice,
+
+    }
+    Dictionary<ParamTextSubsring, string> MTGHunt;
+
+	public BDVKTextParams()
+	{
+        SetMTGHunt();
+
+    }
+
+    private void SetMTGHunt()
+    {
+        MTGHunt = new Dictionary<ParamTextSubsring, string>
+        {
+            [ParamTextSubsring.EndName] = "\nСостояние лота:",
+            [ParamTextSubsring.EndNowPrice] = " рублей)",
+            [ParamTextSubsring.EndPrice] = "\nМинимальный шаг:",
+            [ParamTextSubsring.EndTime] = @"(МСК)",
+            [ParamTextSubsring.StartName] = @"Описание лота: ",
+            [ParamTextSubsring.StartNowPrice] = "(",
+            [ParamTextSubsring.StartPrice] = @"Стартовая цена:",
+            [ParamTextSubsring.StartTime] = @"Дата и время окончания: "
+        }; 
+    }
+}
